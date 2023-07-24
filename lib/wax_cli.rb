5998047cb42e1d9f455b5a01fc12ef3c70c68987
mnyrop <marii@nyu.edu>
2023-07-24 18:29:57 -0400

    cleanup

---
+++ lib/wax_cli.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "wax_cli/version"
+
+module WaxCli
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/wax_cli/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module WaxCli
+  VERSION = "0.1.0"
+end
